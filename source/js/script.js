349e1b614363a8b77b53988aff543461711f2b1f
Shoun <shooouuun@gmail.com>
2019-05-28 17:51:39 +0300

    Свёрстана адаптивная навигация. Учтены параметры с JS и без JS.

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+var toggle__nav = document.querySelector(".page-header__trigger"),
+    nav = document.querySelector(".page-header__navigation-bottom");
+    list = document.querySelector(".page-header__list--extra");
+
+    toggle__nav.classList.remove("page-header__trigger--nojs");
+    nav.classList.remove("page-header__navigation-bottom--nojs");
+    list.classList.add("page-header__list--js");
+
+    toggle__nav.addEventListener("click", function (e) {
+      e.preventDefault();
+      nav.classList.toggle("page-header__navigation-bottom--open");
+      toggle__nav.classList.toggle("page-header__trigger--close");
+  });
