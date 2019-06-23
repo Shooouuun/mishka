73910fe1316c12bf2a8f4f0ee05ba492f2a7fb7d
Shoun <shooouuun@gmail.com>
2019-06-23 05:07:12 +0300

    Свёрстана полностью главная страница, модальное окно и стилизация

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 var toggle__nav = document.querySelector(".page-header__trigger"),
     nav = document.querySelector(".page-header__navigation-bottom");
     list = document.querySelector(".page-header__list--extra");
+    modal__trigger = document.querySelector(".modal-js-button");
+    modal__overlay = document.querySelector(".modal-cart__overlay");
+    modal__wrapper = document.querySelector(".modal-cart__wrapper")
 
     toggle__nav.classList.remove("page-header__trigger--nojs");
     nav.classList.remove("page-header__navigation-bottom--nojs");
@@ -10,4 +13,10 @@ var toggle__nav = document.querySelector(".page-header__trigger"),
       e.preventDefault();
       nav.classList.toggle("page-header__navigation-bottom--open");
       toggle__nav.classList.toggle("page-header__trigger--close");
+    });
+
+    modal__trigger.addEventListener("click", function (e) {
+      e.preventDefault();
+      modal__overlay.classList.add("modal-cart__overlay--open");
+      modal__wrapper.classList.add("modal-cart__wrapper--open")
     });
